c65343bc4555d2013a80072914a58cbf4092cc69
Batuhan Erenler <40266535+batuhanerenler@users.noreply.github.com>
2023-03-01 16:20:49 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,77 +1,64 @@
-import cv2
 import streamlit as st
-from mtcnn import MTCNN
-from deepface import DeepFace
+import cv2
 
-# set page title and icon
-st.set_page_config(page_title="Face Detection App", page_icon=":guardsman:")
+# Load the pre-trained face detection model from OpenCV
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
+# Load the pre-trained age and gender prediction models from OpenCV
+age_net = cv2.dnn.readNetFromCaffe("deploy_age.prototxt", "age_net.caffemodel")
+gender_net = cv2.dnn.readNetFromCaffe("deploy_gender.prototxt", "gender_net.caffemodel")
 
-# function to detect faces using MTCNN
+# Define a function to detect faces, mark them with a square, and predict age and gender
 def detect_faces(image):
-    detector = MTCNN()
-    result = detector.detect_faces(image)
-    if result != []:
-        for person in result:
-            bounding_box = person['box']
-            keypoints = person['keypoints']
-            cv2.rectangle(
-                image,
-                (bounding_box[0], bounding_box[1]),
-                (bounding_box[0]+bounding_box[2], bounding_box[1]+bounding_box[3]),
-                (0, 155, 255),
-                2
-            )
-        return image, True
-    else:
-        return image, False
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    # Detect faces in the grayscale image using the face detection model
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-# function to get gender and age using DeepFace
-def get_gender_age(image):
-    try:
-        result = DeepFace.analyze(image, actions=['gender', 'age'])
-        gender = result['gender']
-        age = result['age']
-        return gender, age
-    except:
-        return None, None
+    # Loop through each detected face
+    for (x, y, w, h) in faces:
+        # Draw a square around the face on the original color image
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
+        # Extract the face ROI
+        face_roi = image[y:y+h, x:x+w]
 
-# main function
-def main():
-    st.title("Face Detection App")
+        # Preprocess the face ROI for age and gender prediction
+        blob = cv2.dnn.blobFromImage(face_roi, scalefactor=1.0, size=(227, 227), mean=(78.4263377603, 87.7689143744, 114.895847746), swapRB=False, crop=False)
 
-    # select image from local files
-    image_file = st.file_uploader(
-        "Upload an image to detect faces, gender, and age", 
-        type=['jpg', 'jpeg', 'png']
-    )
+        # Predict the age of the face ROI using the age prediction model
+        age_net.setInput(blob)
+        age_preds = age_net.forward()
+        age = int(age_preds[0].dot(list(range(0, 101))))
 
-    if image_file is not None:
-        image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), 1)
-        st.image(image, channels="BGR", caption="Uploaded Image")
+        # Predict the gender of the face ROI using the gender prediction model
+        gender_net.setInput(blob)
+        gender_preds = gender_net.forward()
+        gender = "Male" if gender_preds[0][0] > gender_preds[0][1] else "Female"
 
-        # detect faces
-        detected_image, face_detected = detect_faces(image)
-        if face_detected:
-            st.success("Faces detected!")
-        else:
-            st.warning("No faces detected.")
+        # Draw the predicted age and gender on the original color image
+        cv2.putText(image, f"{age} years {gender}", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
 
-        # get gender and age
-        gender, age = get_gender_age(detected_image)
-        if gender is not None and age is not None:
-            st.success("Gender and age detected!")
-            st.write("Gender:", gender)
-            st.write("Age:", age)
-        else:
-            st.warning("Could not detect gender and age.")
-    else:
-        st.warning("Upload an image to get started.")
+    # Return the annotated image
+    return image
 
+# Define the streamlit app
+def app():
+    # Set the title and description
+    st.title("Face Detection and Age/Gender Prediction")
+    st.markdown("Upload an image to detect faces and predict age and gender.")
 
-if __name__ == "__main__":
-    main()
+    # Create a file uploader
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-    
+    # If an image was uploaded, display it and process it
+    if uploaded_file is not None:
+        # Load the image
+        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
+
+        # Detect faces, mark them with a square, and predict age and gender
+        annotated_image = detect_faces(image)
+
+        # Display the annotated image
+        st.image(annotated_image
