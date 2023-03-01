8c7095ff903a0da7176f528c7c8c955a4a0f6953
Batuhan Erenler <40266535+batuhanerenler@users.noreply.github.com>
2023-03-01 16:08:34 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,28 +1,77 @@
 import cv2
 import streamlit as st
-from PIL import Image
-import numpy as np
+from mtcnn import MTCNN
+from deepface import DeepFace
 
+# set page title and icon
+st.set_page_config(page_title="Face Detection App", page_icon=":guardsman:")
+
+
+# function to detect faces using MTCNN
 def detect_faces(image):
-    gray = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
-    for (x, y, w, h) in faces:
-        cv2.rectangle(np.array(image), (x, y), (x+w, y+h), (0, 255, 0), 2)
-    return image
+    detector = MTCNN()
+    result = detector.detect_faces(image)
+    if result != []:
+        for person in result:
+            bounding_box = person['box']
+            keypoints = person['keypoints']
+            cv2.rectangle(
+                image,
+                (bounding_box[0], bounding_box[1]),
+                (bounding_box[0]+bounding_box[2], bounding_box[1]+bounding_box[3]),
+                (0, 155, 255),
+                2
+            )
+        return image, True
+    else:
+        return image, False
 
+
+# function to get gender and age using DeepFace
+def get_gender_age(image):
+    try:
+        result = DeepFace.analyze(image, actions=['gender', 'age'])
+        gender = result['gender']
+        age = result['age']
+        return gender, age
+    except:
+        return None, None
+
+
+# main function
 def main():
-    st.set_page_config(page_title="Real Time Face Detection", page_icon=":smiley:", layout="wide")
-    st.title("Real Time Face Detection App")
-    st.markdown("Upload an image and detect faces in real-time")
-    uploaded_image = st.file_uploader("Choose an image", type=['jpg', 'jpeg', 'png'])
-    if uploaded_image is not None:
-        image = Image.open(uploaded_image)
-        st.subheader("Original Image")
-        st.image(image, use_column_width=True)
-        st.subheader("Processed Image")
-        result_image = detect_faces(image)
-        st.image(result_image, use_column_width=True)
+    st.title("Face Detection App")
 
-if __name__ == '__main__':
+    # select image from local files
+    image_file = st.file_uploader(
+        "Upload an image to detect faces, gender, and age", 
+        type=['jpg', 'jpeg', 'png']
+    )
+
+    if image_file is not None:
+        image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), 1)
+        st.image(image, channels="BGR", caption="Uploaded Image")
+
+        # detect faces
+        detected_image, face_detected = detect_faces(image)
+        if face_detected:
+            st.success("Faces detected!")
+        else:
+            st.warning("No faces detected.")
+
+        # get gender and age
+        gender, age = get_gender_age(detected_image)
+        if gender is not None and age is not None:
+            st.success("Gender and age detected!")
+            st.write("Gender:", gender)
+            st.write("Age:", age)
+        else:
+            st.warning("Could not detect gender and age.")
+    else:
+        st.warning("Upload an image to get started.")
+
+
+if __name__ == "__main__":
     main()
+
+    
