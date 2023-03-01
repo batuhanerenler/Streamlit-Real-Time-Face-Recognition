ade2f69baac4bf781e77bf85c0608bc1c5f42160
Batuhan Erenler <40266535+batuhanerenler@users.noreply.github.com>
2023-03-01 15:59:13 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,51 +1,28 @@
 import cv2
 import streamlit as st
-import pyv4l2
+from PIL import Image
+import numpy as np
 
-def detect_faces(video_capture):
-    # Load the cascade classifier for face detection
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+def detect_faces(image):
+    gray = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(np.array(image), (x, y), (x+w, y+h), (0, 255, 0), 2)
+    return image
 
-    # Loop over frames from the video stream
-    while True:
-        # Read a frame from the video stream
-        frame = video_capture.read()
-
-        # Convert the frame to grayscale
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # Detect faces in the frame
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-
-        # Draw rectangles around the detected faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-        # Convert the frame to RGB and yield it to the client
-        yield cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-# Define the main function for the Streamlit app
 def main():
-    # Set the title of the app
-    st.title('Real-time Face Recognition')
+    st.set_page_config(page_title="Real Time Face Detection", page_icon=":smiley:", layout="wide")
+    st.title("Real Time Face Detection App")
+    st.markdown("Upload an image and detect faces in real-time")
+    uploaded_image = st.file_uploader("Choose an image", type=['jpg', 'jpeg', 'png'])
+    if uploaded_image is not None:
+        image = Image.open(uploaded_image)
+        st.subheader("Original Image")
+        st.image(image, use_column_width=True)
+        st.subheader("Processed Image")
+        result_image = detect_faces(image)
+        st.image(result_image, use_column_width=True)
 
-    # Open the video capture device (0 is usually the built-in webcam)
-    video_capture = pyv4l2.Capture(0)
-
-    # Create a placeholder image for the video stream
-    video_display = st.empty()
-
-    # Loop over frames from the video stream and display the resulting frame
-    for frame in detect_faces(video_capture):
-        video_display.image(frame, channels='RGB')
-
-        # Exit the loop if the 'q' key is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Release the video capture device
-    video_capture.close()
-
-# Run the app
 if __name__ == '__main__':
     main()
